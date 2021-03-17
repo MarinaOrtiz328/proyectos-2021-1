5869cd2e635cc8cf1b44ca52fe31310185583925
Marina Ortiz <a329577@uach.mx>
2021-03-17 15:04:40 -0700

    Programa para listar archivos (ls) y programa para listar el contenido de un archivo (cat).

---
+++ catarchivo2.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+	
+
+	FILE *file=NULL;
+	file=fopen(argv[1],"r");
+	char caracter;
+	
+	if(file!=NULL){
+		
+		while((caracter=fgetc(file))!=EOF){
+			
+			printf("%c",caracter);
+		}
+		fclose(file);
+	}else{
+	
+		printf("Error no se puede abrir el archivo\n");
+	
+	}
+
+
+}
